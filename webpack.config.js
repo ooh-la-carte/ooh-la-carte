0a64a35bb030666aedde2fa42ae5b7f973c32ab2
Jake Lee <jacob.lee715@gmail.com>
2018-01-23 16:15:08 -0600

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    rules : [
+      {
+        test : /\.js?/,
+        exclude: /node_modules/,
+        include : SRC_DIR,
+        loader : 'babel-loader',
+        options: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+};
