62bdf39a5006565c28d68b91aad1a2cb5d31a160
mvalentine512 <mvalentine512@gmail.com>
2018-01-24 11:41:02 -0600

    Creates initial scaffolding for db connection db must be created manually first and config file must be created and filled in

--- database/index.js
+++ database/index.js
@@ -1,1 +1,12 @@
 // Initial Commit
+const config = require('../config.js');
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : process.env.DATABASE_HOST || '127.0.0.1',
+    user : process.env.DATABASE_USER || config.dbUser,
+    password : process.env.DATABASE_PASSWORD || config.dbPass,
+    database : process.env.DATABASE_NAME ||  'thesis'
+  }
+});
