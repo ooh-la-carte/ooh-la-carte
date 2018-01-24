cbdc6c835b0d9af788a4e5a500c84e8f842f6808
Jake Lee <jacob.lee715@gmail.com>
2018-01-24 16:10:22 -0600

    Changes for making react router working with other techs

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import App from './components/App'
+import { BrowserRouter, Route } from 'react-router-dom';
+import reducers from './reducers';
+import App from './components/App';
+import ReduxTest from './components/ReduxTest';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>  
-  </Provider>, document.getElementById('app'));
+                  <BrowserRouter>
+                    <MuiThemeProvider>
+                      <div>
+                        <Route exact path="/" component={App} />
+                        <Route exact path="/test" component={ReduxTest} />
+                      </div>
+                    </MuiThemeProvider>
+                  </BrowserRouter>
+                </Provider>, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(bodyParser());
 
+app.use(express.static(path.join(__dirname, '/../react-client/dist')));
 
-app.listen(3000, function() {
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(path.join(__dirname, '/../react-client/dist/index.html')));
+});
+
+app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
