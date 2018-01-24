af0ac174fca862b6d54cd9b9f454c95415f97b94
Jason McLaughlin <jasonstephenm@gmail.com>
2018-01-24 14:49:52 -0600

    Fix initial linting errors

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,15 +1,15 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import App from './components/App'
+import App from './components/App';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
   <MuiThemeProvider>
     <App />
-  </MuiThemeProvider>  
+  </MuiThemeProvider>
   </Provider>, document.getElementById('app'));
