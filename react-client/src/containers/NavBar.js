af809f832705f2b33510a5045fa8ffcef01df4e2
Jake Lee <jacob.lee715@gmail.com>
2018-01-27 11:19:49 -0600

    Added dropdown for login menu, and a popup modal for logging in. Also made app bar dependent on whether a user is logged in or not

--- react-client/src/containers/NavBar.js
+++ react-client/src/containers/NavBar.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import LoginModal from '../components/LoginModal';
 import '../style.scss';
 
-const NavBar = () => (
-    <div className='landingPageImage'>
-      <div className='navBarContainer'>
-        <div className='navBarTitle'><Link to='/' style={{ color: 'white' }}>Home</Link></div>
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dropdown: false };
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+  }
 
-        <span className='navBarLink'><Link to='/browseEvents' style={{ color: 'white' }}>Events</Link></span>
-        <span className='navBarLink'><Link to='/browseChefs' style={{ color: 'white' }}>Chefs</Link></span>
-        <span className='navBarLink'><Link to='/chat' style={{ color: 'white' }}>Chat</Link></span>
+  toggleDropDown() {
+    console.log(this.state.dropdown);
+    this.setState({ dropdown: !this.state.dropdown });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='navBarContainer'>
+          {localStorage.getItem('userId')
+            ? <div className='navBarTitle'><Link to='/' style={{ color: 'white' }}>Home</Link></div>
+            : <div className='navBarTitle'><Link to='/' style={{ color: 'white' }}>Ooh La Carte</Link></div>
+          }
+          {localStorage.getItem('userId')
+            ? <span className='navBarLink'><Link to='/browseEvents' style={{ color: 'white' }}>Events</Link></span>
+            : null
+          }
+          {localStorage.getItem('userId')
+            ? <span className='navBarLink'><Link to='/browseChefs' style={{ color: 'white' }}>Chefs</Link></span>
+            : null
+          }
+          {localStorage.getItem('userId')
+            ? <span className='navBarLink'><Link to='/chat' style={{ color: 'white' }}>Chat</Link></span>
+            : null
+          }
+          {localStorage.getItem('userId')
+            ? null
+            :
+              <span className='navBarLogin' >
+                <a className='loginDropdownText' onClick={this.toggleDropDown}>Login</a>
+                {this.state.dropdown
+                  ?
+                    <div className='loginDropdown'>
+                      <div className='dropdownLinkContainer'>
+                        <LoginModal />
+                      </div>
+                      <div>
+                        <a className='loginLink'>Sign up</a>
+                      </div>
+                    </div>
+                  : null
+                }
+              </span>
+          }
+        </div>
       </div>
-    </div>
-);
+    );
+  }
+}
 
 
 export default NavBar;
