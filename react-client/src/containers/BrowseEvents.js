0501d011544ec78d969295821e2901a85c48454a
Jake Lee <jacob.lee715@gmail.com>
2018-01-26 19:18:19 -0600

    Added landing page and several others formatted for PWA

---
+++ react-client/src/containers/BrowseEvents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import '../style.scss';
+import { Link } from 'react-router-dom';
+
+const BrowseEvents = () => (
+    <div>
+      <h1>Browse Events</h1>
+    </div>
+);
+
+
+export default BrowseEvents;
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -5,15 +5,19 @@ import { createStore, applyMiddleware } from 'redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
-import App from './components/App';
+import NavBar from './containers/NavBar';
+import LandingPage from './components/LandingPage';
+import BrowseEvents from './containers/BrowseEvents';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
                   <BrowserRouter>
                     <MuiThemeProvider>
-                      <div>
-                        <Route exact path="/" component={App} />
+                      <div id='window'>
+                        <Route path='/' component={NavBar} />
+                        <Route exact path='/' component={LandingPage} />
+                        <Route exact path='/browseEvents' component={BrowseEvents}/>
                       </div>
                     </MuiThemeProvider>
                   </BrowserRouter>
