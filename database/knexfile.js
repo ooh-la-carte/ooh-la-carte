0cc6fd18c19ab89977a6cac6d1139c1066cc6fa2
mvalentine512 <mvalentine512@gmail.com>
2018-01-29 16:54:45 -0600

    refactor the db connections to support different environments and databases.

---
+++ database/knexfile.js
@@ -0,0 +1,23 @@
+
+const config = {
+  client: process.env.DATABASE_ENGINE || 'postgresql',
+  migrations: { tableName: 'knex_migrations' },
+};
+
+if (process.env.DATABASE_ENGINE === 'sqlite3') {
+  config.connection = { filename: process.env.DATABASE_NAME };
+} else {
+  config.connection = {
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+  };
+}
+
+if (process.env.DATABASE_PASSWORD) {
+  config.connection.password = process.env.DATABASE_PASSWORD;
+}
+
+if (process.env.DATABASE_USER) {
+  config.connection.user = process.env.DATABASE_USER;
+}
+module.exports = config;
