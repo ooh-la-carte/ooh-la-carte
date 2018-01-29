6bea5db24c4868a29555318f199e26dff3a41f3b
Jake Lee <jacob.lee715@gmail.com>
2018-01-29 14:53:59 -0600

    Now have a current page navigation bar

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import NavBar from './containers/NavBar';
 import LandingPage from './components/LandingPage';
 import BrowseEvents from './containers/BrowseEvents';
 import BrowseChefs from './containers/BrowseChefs';
+import CurrentPageNavBar from './containers/CurrentPageNavBar';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
                   <BrowserRouter>
                       <div id='window'>
+                        <Route path='/' component={CurrentPageNavBar} />
                         <Route path='/' component={NavBar} />
                         <Route exact path='/' component={LandingPage} />
                         <Route exact path='/browseEvents' component={BrowseEvents}/>
