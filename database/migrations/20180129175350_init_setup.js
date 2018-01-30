98d503def602aec47412c9477c9d9e5183100f82
mvalentine512 <mvalentine512@gmail.com>
2018-01-30 12:25:19 -0600

    untested pushing to branch for review

--- database/migrations/20180129175350_init_setup.js
+++ database/migrations/20180129175350_init_setup.js
@@ -2,26 +2,58 @@ exports.up = (knex, Promise) => {
   Promise.all([
     knex.schema.createTable('users', (table) => {
       table.increments('id').unsigned().primary();
-      table.string('name', 63);
-      table.string('username', 31).unique();
-      table.string('password');
-      table.string('email');
+      table.boolean('is_chef').defaultTo(false);
+      table.string('name');
+      table.string('username').unique().notNullable();
+      table.string('password').notNullable();
+      table.string('email').unique().notNullable();
       table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
       table.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
     }),
 
     knex.schema.createTable('phone_numbers', (table) => {
       table.increments('id').unsigned().primary();
-      table.string('number', 31);
-      table.integer('user_id').unsigned().references('users.id');
+      table.string('number').notNullable();
+      table.integer('user_id').notNullable().references('users.id');
     }),
 
-    knex.schema.createTable('conversations', () => {
-
+    knex.schema.createTable('conversation', (table) => {
+      table.increments('id').unsigned().primary();
     }),
 
-    knex.schema.createTable('events', () => {
+    knex.schema.createTable('chat_subscriber', (table) => {
+      table.integer('user_id').references('users.id');
+      table.integer('conversation_id').references('conversations.id');
+    }),
 
+    knex.schema.createTable('messages', (table) => {
+      table.increments('id').unsigned().primary();
+      table.integer('conversation_id').unsigned().references('conversations.id');
+      table.text('text');
+    }),
+
+    knex.schema.createTable('events', (table) => {
+      table.increments('id').unsigned().primary();
+      table.integer('creator_id').unsigned().notNullable().references('users.id');
+      table.integer('chef_id').unsigned().references('users.id');
+      table.string('name');
+      table.integer('party_size').unsigned().notNullable();
+      table.string('address');
+      table.decimal('budget', 2, 10);
+      table.string('cuisine_type');
+      table.timestamp('date_time');
+      table.string('meal_type');
+      table.text('description');
+      table.text('requests');
+    }),
+
+    knex.schema.createTable('menu', (table) => {
+      table.increments('id').unsigned().primary();
+      table.integer('chef_id').unsigned();
+      table.string('pic');
+      table.text('description');
+      table.string('cuisine_type');
+      table.decimal('price', 2, 10);
     }),
   ]);
 };
