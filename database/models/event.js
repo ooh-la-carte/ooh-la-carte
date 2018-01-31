148dc2c6eabff68021a3c79a6d63a1a4d78c0e12
mvalentine512 <mvalentine512@gmail.com>
2018-01-31 17:26:15 -0600

    Add database insert for inserts

--- database/models/event.js
+++ database/models/event.js
@@ -5,8 +5,16 @@ Event.findAllEvents = userId => (
   knex('events').where({ creator_id: userId }).then()
 );
 
-Event.insertUser = () => (
-  
+Event.insertUser = ({ hostId, date, location, partySize, meal, cuisine, description }) => (
+  knex('events').insert({
+    creator_id: hostId,
+    party_size: partySize,
+    address: location,
+    cuisine_type: cuisine,
+    date_type: date,
+    meal_type: meal,
+    description,
+  }).then().catch(err => console.log('error inserting event into database: ', err))
 );
 
 module.exports = Event;
