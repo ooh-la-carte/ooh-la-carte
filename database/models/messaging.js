34a59d29e49a7d03ab185c813fbc51e85726c5e0
Jake Lee <jacob.lee715@gmail.com>
2018-02-03 10:04:07 -0600

    Chef chat

--- database/models/messaging.js
+++ database/models/messaging.js
@@ -18,16 +18,16 @@ Messaging.getConvosClient = userId => (
   knex('conversations').where('user_id', userId)
 );
 
-Messaging.insertUser = ({ hostId, date, location, partySize, meal, cuisine, description }) => (
-  knex('events').insert({
-    creator_id: hostId,
-    party_size: partySize,
-    address: location,
-    cuisine_type: cuisine,
-    date_type: date,
-    meal_type: meal,
-    description,
-  }).then().catch(err => console.log('error inserting event into database: ', err))
-);
+// Messaging.insertUser = ({ hostId, date, location, partySize, meal, cuisine, description }) => (
+//   knex('events').insert({
+//     creator_id: hostId,
+//     party_size: partySize,
+//     address: location,
+//     cuisine_type: cuisine,
+//     date_type: date,
+//     meal_type: meal,
+//     description,
+//   }).then().catch(err => console.log('error inserting event into database: ', err))
+// );
 
 module.exports = Messaging;
